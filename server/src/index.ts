67d52d69ffbcbd3c8982ef3fcf6498d5bc5c4b11
smartbrapp <lucas@teconsult.com.br>
2026-02-26 18:05:37 -0300

    fix: use dynamic port for deployment

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
-const port = 3333;
+const port = process.env.PORT || 3333;
 
 app.use(cors());
 app.use(express.json());
